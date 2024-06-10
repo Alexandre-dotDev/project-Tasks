5348f13bb64ddc2432019c14140640197561af22
Alexandre JS <alexandre.centro@yahoo.com.br>
2024-06-10 20:23:20 -0300

    feat: add route to delete a task

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectToDatabase = require("./src/database/mongoose.database.js");
-const TaskModel = require("./src/model/task.model.js");
+const TaskModel = require("./src/model/task.model");
 
 dotenv.config();
 
@@ -25,10 +25,25 @@ app.post("/tasks", async (req, res) => {
 
     res.status(201).send(newTask);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error.nessage);
   }
 });
 
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    const taskToDelete = await TaskModel.findById(taskId);
+
+    if (!taskToDelete) return res.status(500).send("Id não localizado.");
+
+    const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+    res.status(200).send(deletedTask);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 const port = process.env.PORT;
 
 app.listen(port, () => {
